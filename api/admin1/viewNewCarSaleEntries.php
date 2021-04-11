6fb0364bcbe05a0206845982ef2b023d7d4990aa
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2021-04-11 17:24:59 +0300

    Add files via upload

---
+++ api/admin1/viewNewCarSaleEntries.php
@@ -0,0 +1,58 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/New_car.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+    // Instantiate new car object
+    $new_car = new New_car($db);
+
+    // new car query
+    $result = $new_car->read();
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check for any new car entries
+    if($num > 0) {
+
+      // array of entries
+      $newCar_arr = array();
+
+      while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $car_entry = array(
+          'VIN' => $VIN,
+          'Manufacturer' => $Manufacturer,
+          'Make' => $Make,
+          'Year' => $Year,
+          'Engine' => $Engine,
+          'Output' => $Output,
+          'No_of_doors' => $No_of_doors,
+          'Fuel_tank_cap' => $Fuel_tank_cap,
+          'Transmission' => $Transmission
+          'Terrain' => $Terrain
+          'Seating_capacity' => $Seating_capacity
+          'Torque' => $Torque
+          'Region' => $Region
+          'DRL' => $DRL
+        );
+
+        // Push to "data"
+        array_push($newCar_arr, $car_entry);
+      }
+
+      // Turn to JSON & output
+      echo json_encode($newCar_arr);
+
+    } else {
+      // No new car entries
+      echo json_encode(
+        array('message' => 'No New Car Sales Entry Found')
+      );
+    }
