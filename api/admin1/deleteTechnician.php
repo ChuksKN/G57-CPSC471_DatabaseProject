c8db179d864e220af9431bfc0248e6fa8a7800e9
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2021-04-12 04:16:07 +0300

    Add files via upload

--- api/admin1/deleteTechnician.php
+++ api/admin1/deleteTechnician.php
@@ -1,20 +1,36 @@
-<?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: DELETE');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
-    include_once '../../models/Admin.php'; // We actually need to include Admin.php and not SalesPerson.php since it will be the admin accessing this (these functions are define in Admin.php)
+    include_once '../../models/Employee.php';
+    include_once '../../models/Technician.php';
 
-    // Instantiate DB & connect
+    //Instantiate DB & connect
     $database = new Database();
     $db = $database->connect();
-    // Instantiate salesperson object
-    $admin = new Admin($db); // Since we defined functions like delete() in Admin.php and must include Admin.php we also have to create the admin object which will perforom the delete function
 
-    // technician Properties
-    $EmployeeID = 01234567892 // EmployeeID is set to int(11) NOT NULL in the SQL dump, we need to add some value for it here when deleting a salesperson
+    // Instantiate sales object
+    $emp = new Employee($db);
+    $tech = new Technician($db);
 
-    $stmt = $admin->delete_technician($EmployeeID); 
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
 
-    ?>
+    // Set EmployeeID to update
+    $emp->EmployeeID = $data->EmployeeID;
+    $tech->EmployeeID = $data->EmployeeID;
+
+    // Delete sales person
+    if($tech->delete() && $emp->delete()){
+        echo json_encode(
+            array('message' => 'Technician Has Been Deleted')
+        );
+    } else{
+        echo json_encode(
+            array('message' => 'Failed To Delete  Technician')
+        );
+    }
