f3fff236e66bf8671b29daab9b8c8d55b355d9a7
AhmedJN <360meahmed@gmail.com>
2021-04-12 16:52:54 -0600

    Add files via upload

--- models/Parts.php
+++ models/Parts.php
@@ -30,51 +30,40 @@
       return $stmt;
     }
 
-    // Get Single Post
-    public function read_single() {
-          // Create query
-          $query = 'SELECT *
-                    FROM ' . $this->table . '
-                    WHERE PartID = ?
-                    LIMIT 0,1';
+    public function read_inventory() {
+      // Create query
+      $query = 'SELECT *
+      FROM ' . $this->table.' 
+      WHERE PartID NOT IN (SELECT DISTINCT u.PartID FROM part_used u)';
 
-          // Prepare statement
-          $stmt = $this->conn->prepare($query);
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
 
-          // Bind ID
-          $stmt->bindParam(1, $this->PartID);
+      // Execute query
+      $stmt->execute();
 
-          // Execute query
-          $stmt->execute();
-
-          $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-          // Set properties
-          $this->Part_Desc = $row['Part_Desc'];
-          $this->PartName = $row['PartName'];
-          $this->Price = $row['Price'];
+      return $stmt;
     }
 
     // Create Post
     public function create() {
           // Create query
-          $query = 'INSERT INTO ' . $this->table . ' SET Part_Desc = :Part_Desc, PartName = :PartName, Price = :Price';
+          $query = 'INSERT INTO ' . $this->table . ' SET PartID = :PartID, Part_Desc = :Part_Desc, PartName = :PartName, Price = :Price';
 
           // Prepare statement
           $stmt = $this->conn->prepare($query);
 
           // Clean data
+          $this->PartID = htmlspecialchars(strip_tags($this->PartID));
           $this->Part_Desc = htmlspecialchars(strip_tags($this->Part_Desc));
           $this->PartName = htmlspecialchars(strip_tags($this->PartName));
           $this->Price = htmlspecialchars(strip_tags($this->Price));
-          $this->Email = htmlspecialchars(strip_tags($this->Email)); //PartID?? in query too??
 
           // Bind data
+          $stmt->bindParam(':PartID', $this->PartID);
           $stmt->bindParam(':Part_Desc', $this->Part_Desc);
           $stmt->bindParam(':PartName', $this->PartName);
           $stmt->bindParam(':Price', $this->Price);
-          $stmt->bindParam(':Email', $this->Email); //partID??
-
           // Execute query
           if($stmt->execute()) {
             return true;
@@ -89,19 +78,19 @@
     // Update Post
     public function update() {
           // Create query
-          $query = 'UPDATE ' . $this->table . '
-                    SET Part_Desc = :Part_Desc, PartName = :PartName, Price = :Price
-                    WHERE PartID = :PartID';
+          $query = 'UPDATE ' . $this->table . ' SET Part_Desc = :Part_Desc, PartName = :PartName, Price = :Price WHERE PartID = :PartID';
 
           // Prepare statement
           $stmt = $this->conn->prepare($query);
 
           // Clean data
+          $this->PartID = htmlspecialchars(strip_tags($this->PartID));
           $this->Part_Desc = htmlspecialchars(strip_tags($this->Part_Desc));
           $this->PartName = htmlspecialchars(strip_tags($this->PartName));
           $this->Price = htmlspecialchars(strip_tags($this->Price));
 
           // Bind data
+          $stmt->bindParam(':PartID', $this->PartID);
           $stmt->bindParam(':Part_Desc', $this->Part_Desc);
           $stmt->bindParam(':PartName', $this->PartName);
           $stmt->bindParam(':Price', $this->Price);
