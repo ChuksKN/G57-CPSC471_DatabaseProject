bd139a0011e64fd9cddb170c1a88598b53b4615d
AhmedJN <360meahmed@gmail.com>
2021-04-12 21:06:41 -0600

    Add files via upload

--- config/Database.php
+++ config/Database.php
@@ -3,9 +3,9 @@
   {
     // DB Params
     private $host = 'localhost';
-    private $db_name = '';
+    private $db_name = 'dealership_dp';
     private $username = 'root';
-    private $password = '';
+    private $password = '12345678';
     private $conn;
 
     // DB Connect
@@ -13,7 +13,7 @@
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
