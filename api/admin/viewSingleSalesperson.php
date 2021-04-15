c06ff0fc176bcadbcb1e60d39c8f947093b4b82b
AhmedJN <360meahmed@gmail.com>
2021-04-15 08:42:29 -0600

    Add files via upload

--- api/admin/viewSingleSalesperson.php
+++ api/admin/viewSingleSalesperson.php
@@ -1,26 +1,23 @@
-<?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-  
-    include_once '../../config/Database.php';
-    include_once '../../models/Salesperson.php';
-  
-    // Instantiate DB & connect 
-    $database = new Database();
-    $db = $database->connect();
-    // Instantiate salesperson object
-    $sales = new Salesperson($db);
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
 
-    // Get EmployeeID
-    $sales->EmployeeID = isset($_GET['EmployeeID']) ? $_GET['EmployeeID'] : die();
+  include_once '../../config/Database.php';
+  include_once '../../models/Salesperson.php';
 
-    // Get sales
-    $sales->read_single();
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
 
-    // Create an array
-    $sales_arr = array();
+  // Instantiate blog post object
+  $sales = new Salesperson($db);
 
+  // Get ID
+  $sales->EmployeeID = isset($_GET['EmployeeID']) ? $_GET['EmployeeID'] : die();
+  echo $sales->EmployeeID;
+  // Get sales
+  $result = $sales->read_single();
     $employee_entry = array(
         'EmployeeID' => $sales->EmployeeID,
         'Fname' => $sales->Fname,
@@ -33,5 +30,5 @@
         'Super_EID' => $sales->Super_EID
     );
 
-    //Make JSON
-    print_r(json_encode($employee_entry));
+    // Turn to JSON & output
+    echo json_encode($employee_entry);
