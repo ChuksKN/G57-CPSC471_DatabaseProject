dbf679afee73d8b0519482fe1b4e7e55e1835126
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2021-04-15 06:01:17 +0300

    Error Handling (Chadi)

--- models/Facilitates_new_sale.php
+++ models/Facilitates_new_sale.php
@@ -14,6 +14,7 @@ class Facilitates_new_sale
   public $LPlateNo;
   public $RegistrationDetails;
   public $Method_of_Payment;
+  public $errormsg = null;
 
   // Constructor with DB
   public function __construct($db)
@@ -71,90 +72,105 @@ class Facilitates_new_sale
   // Create Post
   public function create()
   {
-    // Create query
-    $query = 'INSERT INTO ' . $this->table . ' SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, SaleID = :SaleID, SaleDate = :SaleDate, LPlateNo = :LPlateNo, RegistrationDetails = :RegistrationDetails, Method_of_Payment = :Method_of_Payment';
+    try{
+          // Create query
+        $query = 'INSERT INTO ' . $this->table . ' SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, SaleID = :SaleID, SaleDate = :SaleDate, LPlateNo = :LPlateNo, RegistrationDetails = :RegistrationDetails, Method_of_Payment = :Method_of_Payment';
 
-    // Prepare statement
-    $stmt = $this->conn->prepare($query);
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
 
-    // Clean data
-    $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
-    $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
-    $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-    $this->SaleID = htmlspecialchars(strip_tags($this->SaleID));
-    $this->SaleDate = htmlspecialchars(strip_tags($this->SaleDate));
-    $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-    $this->RegistrationDetails = htmlspecialchars(strip_tags($this->RegistrationDetails));
-    $this->Method_of_Payment = htmlspecialchars(strip_tags($this->Method_of_Payment));
+        // Clean data
+        $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
+        $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
+        $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+        $this->SaleID = htmlspecialchars(strip_tags($this->SaleID));
+        $this->SaleDate = htmlspecialchars(strip_tags($this->SaleDate));
+        $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+        $this->RegistrationDetails = htmlspecialchars(strip_tags($this->RegistrationDetails));
+        $this->Method_of_Payment = htmlspecialchars(strip_tags($this->Method_of_Payment));
 
-    // Bind data
-    $stmt->bindParam(':EmployeeID', $this->EmployeeID);
-    $stmt->bindParam(':CustomerID', $this->CustomerID);
-    $stmt->bindParam(':VIN', $this->VIN);
-    $stmt->bindParam(':SaleID', $this->SaleID);
-    $stmt->bindParam(':SaleDate', $this->SaleDate);
-    $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-    $stmt->bindParam(':RegistrationDetails', $this->RegistrationDetails);
-    $stmt->bindParam(':Method_of_Payment', $this->Method_of_Payment);
+        // Bind data
+        $stmt->bindParam(':EmployeeID', $this->EmployeeID);
+        $stmt->bindParam(':CustomerID', $this->CustomerID);
+        $stmt->bindParam(':VIN', $this->VIN);
+        $stmt->bindParam(':SaleID', $this->SaleID);
+        $stmt->bindParam(':SaleDate', $this->SaleDate);
+        $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+        $stmt->bindParam(':RegistrationDetails', $this->RegistrationDetails);
+        $stmt->bindParam(':Method_of_Payment', $this->Method_of_Payment);
 
-    // Execute query
-    if ($stmt->execute()) {
-      return true;
-    }
+        // Execute query
+        if ($stmt->execute()) {
+          return true;
+        }
 
-    // Print error if something goes wrong
-    printf("Error: %s.\n", $stmt->error);
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
 
-    return false;
+        return false;
+
+        }catch(Exception $e){
+          $this->errormsg = $e->getMessage();
+          return false;
+          
+        }
+    
   }
 
   // Update Post
   public function update()
   {
-    // Create query
-    $query = 'UPDATE ' . $this->table . '
-                    SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, SaleDate = :SaleDate, LPlateNo = :LPlateNo, RegistrationDetails = :RegistrationDetails, Method_of_Payment = :Method_of_Payment
-                    WHERE SaleID = :SaleID';
+    try {
+          // Create query
+        $query = 'UPDATE ' . $this->table . '
+                        SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, SaleDate = :SaleDate, LPlateNo = :LPlateNo, RegistrationDetails = :RegistrationDetails, Method_of_Payment = :Method_of_Payment
+                        WHERE SaleID = :SaleID';
 
-    // Prepare statement
-    $stmt = $this->conn->prepare($query);
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
 
-    // Clean data
-    $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
-    $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
-    $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-    $this->SaleID = htmlspecialchars(strip_tags($this->SaleID));
-    $this->SaleDate = htmlspecialchars(strip_tags($this->SaleDate));
-    $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-    $this->RegistrationDetails = htmlspecialchars(strip_tags($this->RegistrationDetails));
-    $this->Method_of_Payment = htmlspecialchars(strip_tags($this->Method_of_Payment));
+        // Clean data
+        $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
+        $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
+        $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+        $this->SaleID = htmlspecialchars(strip_tags($this->SaleID));
+        $this->SaleDate = htmlspecialchars(strip_tags($this->SaleDate));
+        $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+        $this->RegistrationDetails = htmlspecialchars(strip_tags($this->RegistrationDetails));
+        $this->Method_of_Payment = htmlspecialchars(strip_tags($this->Method_of_Payment));
 
-    // Bind data
-    $stmt->bindParam(':EmployeeID', $this->EmployeeID);
-    $stmt->bindParam(':CustomerID', $this->CustomerID);
-    $stmt->bindParam(':VIN', $this->VIN);
-    $stmt->bindParam(':SaleID', $this->SaleID);
-    $stmt->bindParam(':SaleDate', $this->SaleDate);
-    $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-    $stmt->bindParam(':RegistrationDetails', $this->RegistrationDetails);
-    $stmt->bindParam(':Method_of_Payment', $this->Method_of_Payment);
+        // Bind data
+        $stmt->bindParam(':EmployeeID', $this->EmployeeID);
+        $stmt->bindParam(':CustomerID', $this->CustomerID);
+        $stmt->bindParam(':VIN', $this->VIN);
+        $stmt->bindParam(':SaleID', $this->SaleID);
+        $stmt->bindParam(':SaleDate', $this->SaleDate);
+        $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+        $stmt->bindParam(':RegistrationDetails', $this->RegistrationDetails);
+        $stmt->bindParam(':Method_of_Payment', $this->Method_of_Payment);
 
-    // Execute query
-    if ($stmt->execute()) {
-      return true;
-    }
+        // Execute query
+        if ($stmt->execute()) {
+          return true;
+        }
 
-    // Print error if something goes wrong
-    printf("Error: %s.\n", $stmt->error);
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
 
-    return false;
+        return false;
+
+        }catch(Exception $e){
+          $this->errormsg = $e->getMessage();
+          return false;
+        }
+    
   }
 
   // Delete Post
   public function delete()
   {
-
-    // Create query
+    try {
+       // Create query
     $query = 'DELETE FROM ' . $this->table . ' WHERE SaleID = :SaleID';
 
     // Prepare statement
@@ -175,5 +191,12 @@ class Facilitates_new_sale
     printf("Error: %s.\n", $stmt->error);
 
     return false;
+  }catch(Exception $e){
+    $this->errormsg = $e->getMessage();
+    return false;
   }
+
+    }
+
+   
 }
