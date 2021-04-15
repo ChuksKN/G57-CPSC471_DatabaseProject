21206c41f100b9015ae497d1a77e8e79c6a32cfb
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2021-04-15 07:07:44 +0300

    Error Handling (Chadi)

--- models/Rental_car.php
+++ models/Rental_car.php
@@ -21,6 +21,7 @@ class Rental_car
     public $Region;
     public $DRL;
     public $LPlateNo;
+    public $errormsg = null;
 
     // Constructor with DB
     public function __construct($db)
@@ -81,109 +82,135 @@ class Rental_car
         $this->DRL = $row['DRL'];
     }
 
-    // Get VIN from Post
-    public function check_vin()
-    {
-        // Create query
-        $query = 'SELECT *
-                              FROM car NATURAL JOIN ' . $this->table . '
-                              WHERE VIN = ?';
-
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
-
-        // Bind ID
-        $stmt->bindParam(1, $this->VIN);
-
-        // Execute query
-        $stmt->execute();
-
-        return $stmt;
-    }
-
     // Create Post
     public function create()
     {
-        // Create query
-        $query = 'INSERT INTO ' . $this->table . ' SET VIN = :VIN, LPlateNo = :LPlateNo';
+        try{// Create query
+            $query = 'INSERT INTO ' . $this->table . ' SET VIN = :VIN, LPlateNo = :LPlateNo';
+    
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+    
+            // Clean data
+            $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+            $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+    
+            // Bind data
+            $stmt->bindParam(':VIN', $this->VIN);
+            $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+    
+            // Execute query
+            if ($stmt->execute()) {
+                if($stmt->rowCount()==0){
+                  $this->errormsg = 'No row was effected. Invalid entry.';
+                  return false;
+                }
+              return true;
+              }
+    
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+    
+            return false;
 
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
-
-        // Clean data
-        $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-        $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-
-        // Bind data
-        $stmt->bindParam(':VIN', $this->VIN);
-        $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-
-        // Execute query
-        if ($stmt->execute()) {
-            return true;
+        }catch(Exception $e){
+            $this->errormsg = $e->getMessage();
+            return false;
         }
-
-        // Print error if something goes wrong
-        printf("Error: %s.\n", $stmt->error);
-
-        return false;
+        
     }
 
     // Update Post
     public function update()
     {
-        // Create query
-        $query = 'UPDATE ' . $this->table . '
-                    SET LPlateNo = :LPlateNo
-                    WHERE VIN = :VIN';
+        try{ // Create query
+            $query = 'UPDATE car
+                        SET Manufacturer = :Manufacturer, Make = :Make, `Year` = :`Year`, Engine = :Engine, `Output` = :`Output`, No_of_doors = :No_of_doors, Fuel_tank_cap = :Fuel_tank_cap, Transmission = :Transmission, Terrain = :Terrain, 
+                        Seating_capacity = :Seating_capacity, Torque = :Torque, Region = :Region, DRL = :DRL
+                        WHERE VIN = :VIN';
+    
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+    
+            // Clean data
+            $this->Manufacturer = htmlspecialchars(strip_tags($this->Manufacturer));
+            $this->Make = htmlspecialchars(strip_tags($this->Make));
+            $this->Year = htmlspecialchars(strip_tags($this->Year));
+            $this->Engine = htmlspecialchars(strip_tags($this->Engine));
+            $this->Output = htmlspecialchars(strip_tags($this->Output));
+            $this->No_of_doors = htmlspecialchars(strip_tags($this->No_of_doors));
+            $this->Fuel_tank_cap = htmlspecialchars(strip_tags($this->Fuel_tank_cap));
+            $this->Transmission = htmlspecialchars(strip_tags($this->Transmission));
+            $this->Terrain = htmlspecialchars(strip_tags($this->Terrain));
+            $this->Seating_capacity = htmlspecialchars(strip_tags($this->Seating_capacity));
+            $this->Torque = htmlspecialchars(strip_tags($this->Torque));
+            $this->Region = htmlspecialchars(strip_tags($this->Region));
+            $this->DRL = htmlspecialchars(strip_tags($this->DRL));
+    
+            // Bind data
+            $stmt->bindParam(':VIN', $this->VIN);
+            $stmt->bindParam(':Manufacturer', $this->Manufacturer);
+            $stmt->bindParam(':Make', $this->Make);
+            $stmt->bindParam(':Year', $this->Year);
+            $stmt->bindParam(':Engine', $this->Engine);
+            $stmt->bindParam(':Output', $this->Output);
+            $stmt->bindParam(':No_of_doors', $this->No_of_doors);
+            $stmt->bindParam(':Fuel_tank_cap', $this->Fuel_tank_cap);
+            $stmt->bindParam(':Transmission', $this->Transmission);
+            $stmt->bindParam(':Terrain', $this->Terrain);
+            $stmt->bindParam(':Seating_capacity', $this->Seating_capacity);
+            $stmt->bindParam(':Torque', $this->Torque);
+            $stmt->bindParam(':Region', $this->Region);
+            $stmt->bindParam(':DRL', $this->DRL);
+    
+            // Execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+    
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+    
+            return false;
 
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
-
-        // Clean data
-        $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-        $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-
-        // Bind data
-        $stmt->bindParam(':VIN', $this->VIN);
-        $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-
-        // Execute query
-        if ($stmt->execute()) {
-            return true;
+        }catch(Exception $e){
+            $this->errormsg = $e->getMessage();
+            return false;
         }
-
-        // Print error if something goes wrong
-        printf("Error: %s.\n", $stmt->error);
-
-        return false;
+       
     }
 
     // Delete Post
     public function delete()
     {
-        // Create query
-        $query = 'DELETE FROM ' . $this->table . ' WHERE VIN = :VIN, LPlateNo = :LPlateNo';
-
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
-
-        // Clean data
-        $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-        $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-
-        // Bind data
-        $stmt->bindParam(':VIN', $this->VIN);
-        $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-
-        // Execute query
-        if ($stmt->execute()) {
-            return true;
+        try{// Create query
+            $query = 'DELETE FROM ' . $this->table . ' WHERE VIN = :VIN, LPlateNo = :LPlateNo';
+    
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+    
+            // Clean data
+            $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+            $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+    
+            // Bind data
+            $stmt->bindParam(':VIN', $this->VIN);
+            $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+    
+            // Execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+    
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+    
+            return false;
+        }catch(Exception $e){
+            $this->errormsg = $e->getMessage();
+            return false;
         }
 
-        // Print error if something goes wrong
-        printf("Error: %s.\n", $stmt->error);
-
-        return false;
-    }
+        }
+        
 }
