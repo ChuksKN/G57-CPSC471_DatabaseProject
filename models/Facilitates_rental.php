b9f749962e2174469b18dccdebcd95763df609f1
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2021-04-15 06:18:25 +0300

    Error Handling (Chadi)

--- models/Facilitates_rental.php
+++ models/Facilitates_rental.php
@@ -15,6 +15,7 @@ class Facilitates_rental
   public $PaymentMethod;
   public $StartDate;
   public $ReturnDate;
+  public $errormsg = null;
 
   // Constructor with DB
   public function __construct($db)
@@ -72,117 +73,139 @@ class Facilitates_rental
   // Create Post
   public function create()
   {
-    // Create query
-    $query = 'INSERT INTO ' . $this->table . ' SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, RentalID = :RentalID, StartDate = :StartDate, ReturnDate = :ReturnDate, LPlateNo = :LPlateNo, RentalDetails = :RentalDetails, PaymentMethod = :PaymentMethod';
+    try{
+            // Create query
+          $query = 'INSERT INTO ' . $this->table . ' SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, RentalID = :RentalID, StartDate = :StartDate, ReturnDate = :ReturnDate, LPlateNo = :LPlateNo, RentalDetails = :RentalDetails, PaymentMethod = :PaymentMethod';
 
-    // Prepare statement
-    $stmt = $this->conn->prepare($query);
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
 
-    // Clean data
-    $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
-    $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
-    $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-    $this->RentalID = htmlspecialchars(strip_tags($this->RentalID));
-    $this->StartDate = htmlspecialchars(strip_tags($this->StartDate));
-    $this->ReturnDate = htmlspecialchars(strip_tags($this->ReturnDate));
-    $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-    $this->RentalDetails = htmlspecialchars(strip_tags($this->RentalDetails));
-    $this->PaymentMethod = htmlspecialchars(strip_tags($this->PaymentMethod));
+          // Clean data
+          $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
+          $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
+          $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+          $this->RentalID = htmlspecialchars(strip_tags($this->RentalID));
+          $this->StartDate = htmlspecialchars(strip_tags($this->StartDate));
+          $this->ReturnDate = htmlspecialchars(strip_tags($this->ReturnDate));
+          $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+          $this->RentalDetails = htmlspecialchars(strip_tags($this->RentalDetails));
+          $this->PaymentMethod = htmlspecialchars(strip_tags($this->PaymentMethod));
 
-    // Bind data
-    $stmt->bindParam(':EmployeeID', $this->EmployeeID);
-    $stmt->bindParam(':CustomerID', $this->CustomerID);
-    $stmt->bindParam(':VIN', $this->VIN);
-    $stmt->bindParam(':RentalID', $this->RentalID);
-    $stmt->bindParam(':StartDate', $this->StartDate);
-    $stmt->bindParam(':ReturnDate', $this->ReturnDate);
-    $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-    $stmt->bindParam(':RentalDetails', $this->RentalDetails);
-    $stmt->bindParam(':PaymentMethod', $this->PaymentMethod);
+          // Bind data
+          $stmt->bindParam(':EmployeeID', $this->EmployeeID);
+          $stmt->bindParam(':CustomerID', $this->CustomerID);
+          $stmt->bindParam(':VIN', $this->VIN);
+          $stmt->bindParam(':RentalID', $this->RentalID);
+          $stmt->bindParam(':StartDate', $this->StartDate);
+          $stmt->bindParam(':ReturnDate', $this->ReturnDate);
+          $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+          $stmt->bindParam(':RentalDetails', $this->RentalDetails);
+          $stmt->bindParam(':PaymentMethod', $this->PaymentMethod);
 
-    // Execute query
-    if ($stmt->execute()) {
-      return true;
+          // Execute query
+          if ($stmt->execute()) {
+            return true;
+          }
+
+          // Print error if something goes wrong
+          printf("Error: %s.\n", $stmt->error);
+
+          return false;
+
+    }catch(Exception $e){
+      $this->errormsg = $e->getMessage();
+      return false;
     }
-
-    // Print error if something goes wrong
-    printf("Error: %s.\n", $stmt->error);
-
-    return false;
+    
   }
 
   // Update Post
   public function update()
   {
-    // Create query
-    $query = 'UPDATE ' . $this->table . '
-                    SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, StartDate = :StartDate, ReturnDate = :ReturnDate, RentalDetails = :RentalDetails, LPlateNo = :LPlateNo, PaymentMethod = :PaymentMethod
-                    WHERE RentalID = :RentalID';
+    try{
+            // Create query
+          $query = 'UPDATE ' . $this->table . '
+          SET EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN, StartDate = :StartDate, ReturnDate = :ReturnDate, RentalDetails = :RentalDetails, LPlateNo = :LPlateNo, PaymentMethod = :PaymentMethod
+          WHERE RentalID = :RentalID';
 
-    // Prepare statement
-    $stmt = $this->conn->prepare($query);
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
 
-    // Clean data
-    $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
-    $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
-    $this->VIN = htmlspecialchars(strip_tags($this->VIN));
-    $this->RentalID = htmlspecialchars(strip_tags($this->RentalID));
-    $this->StartDate = htmlspecialchars(strip_tags($this->StartDate));
-    $this->ReturnDate = htmlspecialchars(strip_tags($this->ReturnDate));
-    $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
-    $this->RentalDetails = htmlspecialchars(strip_tags($this->RentalDetails));
-    $this->PaymentMethod = htmlspecialchars(strip_tags($this->PaymentMethod));
+          // Clean data
+          $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
+          $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
+          $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+          $this->RentalID = htmlspecialchars(strip_tags($this->RentalID));
+          $this->StartDate = htmlspecialchars(strip_tags($this->StartDate));
+          $this->ReturnDate = htmlspecialchars(strip_tags($this->ReturnDate));
+          $this->LPlateNo = htmlspecialchars(strip_tags($this->LPlateNo));
+          $this->RentalDetails = htmlspecialchars(strip_tags($this->RentalDetails));
+          $this->PaymentMethod = htmlspecialchars(strip_tags($this->PaymentMethod));
 
-    // Bind data
-    $stmt->bindParam(':EmployeeID', $this->EmployeeID);
-    $stmt->bindParam(':CustomerID', $this->CustomerID);
-    $stmt->bindParam(':VIN', $this->VIN);
-    $stmt->bindParam(':RentalID', $this->RentalID);
-    $stmt->bindParam(':StartDate', $this->StartDate);
-    $stmt->bindParam(':ReturnDate', $this->ReturnDate);
-    $stmt->bindParam(':LPlateNo', $this->LPlateNo);
-    $stmt->bindParam(':RentalDetails', $this->RentalDetails);
-    $stmt->bindParam(':PaymentMethod', $this->PaymentMethod);
+          // Bind data
+          $stmt->bindParam(':EmployeeID', $this->EmployeeID);
+          $stmt->bindParam(':CustomerID', $this->CustomerID);
+          $stmt->bindParam(':VIN', $this->VIN);
+          $stmt->bindParam(':RentalID', $this->RentalID);
+          $stmt->bindParam(':StartDate', $this->StartDate);
+          $stmt->bindParam(':ReturnDate', $this->ReturnDate);
+          $stmt->bindParam(':LPlateNo', $this->LPlateNo);
+          $stmt->bindParam(':RentalDetails', $this->RentalDetails);
+          $stmt->bindParam(':PaymentMethod', $this->PaymentMethod);
+          $stmt->bindParam(':EmployeeID', $this->EmployeeID);
 
-    // Execute query
-    if ($stmt->execute()) {
-      return true;
+          // Execute query
+          if ($stmt->execute()) {
+          return true;
+          }
+
+          // Print error if something goes wrong
+          printf("Error: %s.\n", $stmt->error);
+
+          return false;
+
+    }catch(Exception $e){
+      $this->errormsg = $e->getMessage();
+      return false;
     }
-
-    // Print error if something goes wrong
-    printf("Error: %s.\n", $stmt->error);
-
-    return false;
+    
   }
 
   // Delete Post
   public function delete()
   {
+    try{
+            // Create query
+          $query = 'DELETE FROM ' . $this->table . ' WHERE EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN';
 
-    // Create query
-    $query = 'DELETE FROM ' . $this->table . ' WHERE EmployeeID = :EmployeeID, CustomerID = :CustomerID, VIN = :VIN';
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
 
-    // Prepare statement
-    $stmt = $this->conn->prepare($query);
+          // Clean data
+          $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
+          $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
+          $this->VIN = htmlspecialchars(strip_tags($this->VIN));
 
-    // Clean data
-    $this->EmployeeID = htmlspecialchars(strip_tags($this->EmployeeID));
-    $this->CustomerID = htmlspecialchars(strip_tags($this->CustomerID));
-    $this->VIN = htmlspecialchars(strip_tags($this->VIN));
+          // Bind data
+          $stmt->bindParam(':EmployeeID', $this->EmployeeID);
+          $stmt->bindParam(':CustomerID', $this->CustomerID);
+          $stmt->bindParam(':VIN', $this->VIN);
 
-    // Bind data
-    $stmt->bindParam(':EmployeeID', $this->EmployeeID);
-    $stmt->bindParam(':CustomerID', $this->CustomerID);
-    $stmt->bindParam(':VIN', $this->VIN);
+          // Execute query
+          if ($stmt->execute()) {
+            return true;
+          }
 
-    // Execute query
-    if ($stmt->execute()) {
-      return true;
+          // Print error if something goes wrong
+          printf("Error: %s.\n", $stmt->error);
+
+          return false;
+        }catch(Exception $e){
+          $this->errormsg = $e->getMessage();
+          return false;
+        }
+
     }
 
-    // Print error if something goes wrong
-    printf("Error: %s.\n", $stmt->error);
-
-    return false;
-  }
+    
 }
